ca63f0ca4fd4a2d572f114a4295e67afa9c7190d
Daniel Dickson Okorodudu <dcodejava@gmail.com>
2016-08-22 18:11:16 +0100

    Integrate Crypt by Rijndeal

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright (c) 2008, 2010, 2016, JavaDan and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * Created using PhpStorm.
+ * @package Roli
+ *
+ * @author Java <dcodejava@gmail.com>
+ * @time-stamp 22/08/2016, 03:12 PM.
+ */
+require_once 'vendor/autoload.php';
+
+$fb = new Facebook\Facebook([
+    'app_id' => '290233574680993', // Replace {app-id} with your app id
+    'app_secret' => '04375d7110ec57f9d2d9882b3b01a24c',
+    'default_graph_version' => 'v2.2',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+$callback = $_SERVER['SERVER_ADDR'] . $_SERVER['PHP_SELF'];
+$callback = 'http://localhost/roli/index.php';
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl($callback, $permissions);
+
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+
+$rijndael = new \phpseclib\Crypt\Rijndael();
+
+$rijndael->setKey('abcdefghijklmnop');
+
+$size = 10 * 1024;
+$plaintext = '';
+for ($i = 0; $i < $size; $i++) {
+    $plaintext .= 'a';
+}
+
+echo "<h1>" . $rijndael->decrypt($rijndael->encrypt($plaintext)) . "</h1>";
